3b59d86b58b43f7c6036d88a6df5bac21d3d2055
Eliud Kagema <eliud.k.maina@gmail.com>
2020-06-18 23:02:46 +0300

    first commit

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask, jsonify, request
-import requests 
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Integer, String, Text, Binary, Column
+import os
 
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'planets.db')
+
+
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello_world():
@@ -27,9 +34,21 @@ def parameters():
         return jsonify(message="Welcome " + name + ", to planetary.")
 
 
-@app.route('/url_variables')
-def url_variables():
-    return jsonify(message='That resource does not exist'), 404
+@app.route('/url_variables/<string:name>/<int:age>')
+def url_variables(name: str, age:int):
+    if age < 18:
+        return jsonify(message="Sorry " + name + ", You are not old enough."), 401
+    else:
+        return jsonify(message="Welcome " + name + ", to planetary.")
+
+
+
+# Data Models
+class User(db.Model):
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    email = Column()
 
 
 if __name__ == '__main__':
